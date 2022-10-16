43694d334c4774b8da64fd65bbdbcabb0423ee8a
gautam-kartikey-2000 <115928114+gautam-kartikey-2000@users.noreply.github.com>
2022-10-16 16:56:26 +0530

    Add files via upload

---
+++ blood_bank_register.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const axios = require('axios');
+var uuid = require('uuid');
+
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import path from 'path';
+// import bcrypt from 'bcryptjs';
+// import jwt from 'jsonwebtoken';
+// import  config  from 'config';
+// import { check, validationResult } from 'express-validator';
+// import userSchema from '../models/user.js'
+
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+//const normalize = require('normalize-url');
+
+//console.log(uuid.v4());
+const Blood_bank_model = require('../models/blood_bank');
+
+
+router.post(
+ '/',
+ check('Name', 'Name is required').exists(),
+ check('Email', 'Please include a valid email').exists(),
+ check(
+   'Password',
+   'Please enter a password with 6 or more characters'
+ ).isLength({ min: 6 }),
+ check('City', 'Residence City is required'),
+ check('State', 'Residence state is required'),
+ check('Pincode', 'Residence pincode is required'),
+ async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+
+   const  name = req.body.Name;
+   const email = req.body.Email;
+   const password  = req.body.Password;
+   const city = req.body.City;
+   const state = req.body.State;
+   const pincode = req.body.Pincode;
+   const phone_number = req.body.Phone_number;
+   const bankId =  uuid.v4();
+   //let locations;
+   console.log(name,email,password);
+
+   const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ city + " " + state +'.json?access_token=pk.eyJ1IjoibWFoYWstcmF3YXQiLCJhIjoiY2tra3FpZjN1MDNoMjJ3bG9sdDdhdTY0ayJ9.zaTDuw_EF0IjEd3e8jwiQQ&limit=1'
+    await axios.get(url)
+    .then(({data})=>{
+        
+        req.body.location={type:'Point',coordinates:[data.features[0].center[0],data.features[0].center[1]]}
+        console.log(req.body.location.type);})
+        
+    .catch((err)=>{res.status(400).send(err)});
+
+   try {
+     let user = await Blood_bank_model.findOne({ email : email });
+
+     if (user) {
+       return res
+         .status(400)
+         .json({ errors: [{ msg: 'User already exists' }] });
+     }
+
+   //   const avatar = normalize(
+   //     gravatar.url(email, {
+   //       s: '200',
+   //       r: 'pg',
+   //       d: 'mm'
+   //     }),
+   //     { forceHttps: true }
+   //   );
+
+     console.log(req.body.location);
+     let locations =req.body.location;
+     user = new Blood_bank_model({
+       name : name,
+       phone_number: phone_number,
+       email :email,
+       password:password,
+       location :{
+       city: city,
+       state: state,
+       pincode: pincode,
+       },
+       location_coordinates: locations,
+       bankId: bankId
+   
+
+     });
+
+    //  const salt = await bcrypt.genSalt(10);
+
+    //  user.password = await bcrypt.hash(password, salt);
+
+     await user.save();
+
+     const payload = {
+        user: {
+          id: user.id,
+          type: 2
+        }
+      };
+
+     jwt.sign(
+       payload,
+       'jwtSecret',
+
+       (err, token) => {
+         if (err) throw err;
+         res.json({ token,user });
+       }
+    );
+   } catch (err) {
+     console.error(err.message);
+     res.status(500).send('Server error');
+   }
+ }
+);
+
+module.exports = router ;
